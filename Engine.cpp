f8fc4f55204d07b7303327a265f635c3cbbd58ba
SpontanCombust <cedro_p@o2.pl>
2021-10-11 17:43:02 +0200

    Moved init code

--- Engine.cpp
+++ Engine.cpp
@@ -4,13 +4,9 @@
 
 Engine::Engine(const Window& window)
 {
-	if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS))
-	{
-		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
 	
-	sdl_window = SDL_CreateWindow(window.title, window.x, window.y, window.w, window.h, window.window_display_type);
+	
+	sdl_window = SDL_CreateWindow(window.title.c_str(), window.x, window.y, window.w, window.h, window.window_display_type);
 }
 	
 Engine::~Engine()
