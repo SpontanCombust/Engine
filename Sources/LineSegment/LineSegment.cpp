f9efa1fa902afd52f94705aed654d783ce1b9bb8
SpontanCombust <cedro_p@o2.pl>
2021-10-25 15:23:05 +0200

    find sdl and line segment

--- Sources/LineSegment/LineSegment.cpp
+++ Sources/LineSegment/LineSegment.cpp
@@ -6,8 +6,26 @@ LineSegment::LineSegment(Point2D & p0, Point2D & p1)
     this->p1 = p1;
 }
 
-void get_end_points(Point2D & p0, Point2D & p1)
+void LineSegment::get_end_points(Point2D & p0, Point2D & p1)
 {
     p0 = this->p0;
     p1 = this->p1;
+}
+
+void LineSegment::set_end_points(Point2D & p0, Point2D & p1) 
+{
+    this->p0 = p0;
+    this->p1 = p1;
+}
+
+void LineSegment::draw(DrawAlgorithmType algorithm_type) 
+{
+    if( algorithm_type == DrawAlgorithmType::NAIVE )
+    {
+        PrimitiveRenderer::naively_draw_line( p0.get_x(), p0.get_y(), p1.get_x(), p1.get_y() );
+    }
+    else
+    {
+        PrimitiveRenderer::draw_line( p0.get_x(), p0.get_y(), p1.get_x(), p1.get_y() );
+    }
 }
--- Sources/LineSegment/LineSegment.hpp
+++ Sources/LineSegment/LineSegment.hpp
@@ -2,14 +2,20 @@
 
 #include "Point2D/Point2D.hpp"
 
+enum class DrawAlgorithmType
+{
+    NAIVE,
+    SDL
+};
+
 class LineSegment
 {
 public:
     LineSegment(Point2D & p0, Point2D & p1);
 
-    static void get_end_points(Point2D & p0, Point2D & p1);
-    static void set_end_points(Point2D & p0, Point2D & p1);
-    static void draw(AlgorithmType algorithm_type);
+    void get_end_points(Point2D & p0, Point2D & p1);
+    void set_end_points(Point2D & p0, Point2D & p1);
+    void draw(DrawAlgorithmType algorithm_type);
 
 private:
     Point2D p0;
