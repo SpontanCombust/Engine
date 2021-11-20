4e4056353c5f1f111f9e0049caa7443d24053529
SpontanCombust <cedro_p@o2.pl>
2021-11-20 14:33:22 +0100

    NEW Basic game object classes

---
+++ Sources/GameObjects/GameObject.hpp
@@ -0,0 +1,10 @@
+#ifndef __GAMEOBJECT_H__
+#define __GAMEOBJECT_H__
+
+class GameObject
+{
+public:
+    virtual ~GameObject() = default; // interface needs at least virtual destructor
+};
+
+#endif // __GAMEOBJECT_H__
