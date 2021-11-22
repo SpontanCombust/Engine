e1b85bcd47ea46946becd9b2c6c515e4d74aaff6
SpontanCombust <cedro_p@o2.pl>
2021-11-22 15:27:45 +0100

    feat: bitmap renderer

---
+++ Sources/BitmapRenderer/BitmapRenderer.cpp
@@ -0,0 +1,20 @@
+#include "BitmapRenderer.hpp"
+
+SDL_Renderer *BitmapRenderer::sdl_renderer = nullptr;
+
+void BitmapRenderer::setup(SDL_Renderer *renderer) 
+{
+    sdl_renderer = renderer;
+}
+
+void BitmapRenderer::draw_bitmap(SDL_Texture *bitmap, int pos_x, int pos_y, int size_x, int size_y, float rot) 
+{
+    SDL_Rect rect;
+    rect.x = pos_x;
+    rect.y = pos_y;
+    rect.w = size_x;
+    rect.h = size_y;
+
+    // For now it will always draw from the center
+    SDL_RenderCopyEx( sdl_renderer, bitmap, NULL, &rect, rot, NULL, SDL_RendererFlip::SDL_FLIP_NONE );
+}
---
+++ Sources/BitmapRenderer/BitmapRenderer.hpp
@@ -0,0 +1,17 @@
+#ifndef __BITMAPRENDERER_H__
+#define __BITMAPRENDERER_H__
+
+#include <SDL.h>
+
+class BitmapRenderer
+{
+public:
+    static void setup( SDL_Renderer *renderer );
+
+    static void draw_bitmap( SDL_Texture *bitmap, int pos_x, int pos_y, int size_x, int size_y, float rot );
+
+private:
+    static SDL_Renderer *sdl_renderer;
+};
+
+#endif // __BITMAPRENDERER_H__
